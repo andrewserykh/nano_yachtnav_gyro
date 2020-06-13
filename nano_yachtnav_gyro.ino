38ac673991e4a6e3c1571058e3b956e77c7593a3
Andrew Serykh <35666362+andrewserykh@users.noreply.github.com>
2020-06-13 17:14:52 +0700

    GY-521
    Added GY-521 module support,
    button UP - AP on, button DOWN - AP off

--- nano_yachtnav_gyro.ino
+++ nano_yachtnav_gyro.ino
@@ -1,4 +1,4 @@
-/* v1.1
+/* v2.1
 
 !!!Внимание!!! Если bootloader не optiboot, то wdt надо закомментировать, он не работает на стандартной прошивке
 При прошивке: 
@@ -10,6 +10,8 @@ Watchdog таймер
 
 */
 #include <avr/wdt.h>
+#include "gy521.h"
+#include <Wire.h>
 
 #define WDT_OFF         1     //1=Без WatchDog таймера (не optiboot)
 
@@ -26,12 +28,23 @@ Watchdog таймер
 #define IN_BTN4         10    //D10
 
 bool Button1,Button2,Button3,Button4;
+MPU6050 mpu6050(Wire);
+
+bool AP; //состояние автопилота (включен/выключен) (-->LED13)
+float Course; //курс по гиродатчику, долюен быть = 0 (ось z)
 
 void setup() {
   wdt_disable();
-  
+
+  Serial.begin(9600);
+
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(false,100,100);
+
   pinMode(LED, OUTPUT); //D13 LED
   pinMode(LED, LOW);
+    
   pinMode(OUT_RELAY1,OUTPUT);
   digitalWrite(OUT_RELAY1, LOW);
   pinMode(OUT_RELAY2,OUTPUT);
@@ -49,29 +62,51 @@ void setup() {
   digitalWrite(IN_BTN3,HIGH);
   pinMode(IN_BTN4,INPUT_PULLUP);
   digitalWrite(IN_BTN4,HIGH);
- 
+
   if (!WDT_OFF) wdt_enable(WDTO_1S);
   wdt_reset();
 }
 
 void loop() {
 
-  Button1 = ButtonPressed(IN_BTN1);
-  Button2 = ButtonPressed(IN_BTN2);
-  Button3 = ButtonPressed(IN_BTN3);
-  Button4 = ButtonPressed(IN_BTN4);
+  Button1 = ButtonPressed(IN_BTN1); //TURN LEFT SIDE
+  Button2 = ButtonPressed(IN_BTN2); //TURN RIGHT SIDE
+  Button3 = ButtonPressed(IN_BTN3); //JOY UP
+  Button4 = ButtonPressed(IN_BTN4); //JOY DOWN
+
+  digitalWrite( LED, AP ); // led 13 sign autopilot state
 
   digitalWrite( OUT_RELAY1, LOW );
   digitalWrite( OUT_RELAY2, LOW );
 
-  if (Button1) {
+  if (Button1) { //TURN LEFT
     digitalWrite( OUT_RELAY1, HIGH );
     digitalWrite( OUT_RELAY2, LOW );
-  }
-  if (Button2) {
+  } //Button1
+  
+  if (Button2) { // TURN RIGHT
+      Serial.println("RIGHT");
+
     digitalWrite( OUT_RELAY1, LOW );
     digitalWrite( OUT_RELAY2, HIGH );
+  } //Button2
+
+  mpu6050.update();
+  Course = mpu6050.getGyroAngleZ();
+
+  if (Button3) {
+    wdt_disable();
+    mpu6050.calcGyroOffsets(false,100,100);
+    mpu6050.resetAngleZ();
+    AP = true;
+    wdt_enable(WDTO_1S);
+    wdt_reset();
+  } //Button3
+
+  if (Button4) {
+    AP = false;
   }
+
   
   wdt_reset();
 
